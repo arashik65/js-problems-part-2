c5c14014e8c6114cd138560aee0bb2f3d34da46e
arashik65 <mdashikur6398+@gmail.com>
2026-02-08 00:04:13 +0600

    type check

---
+++ validation.js
@@ -0,0 +1,47 @@
+function multiplay (num1 , num2){
+  if(typeof num1 !== 'number' || typeof num2 !== 'number' ){
+    return 'please provide a number';
+  }
+
+    const mult = num1 * num2 ;
+     return mult;
+    
+}
+const result = multiplay('5','seven');
+// console.log(result);
+
+function fullName (first, second){
+    if(typeof first!=='string'){
+        return 'First name should be a strng';
+    }
+   else if (typeof second!== 'string'){
+    return 'Second Name Should be a string';
+   }
+   const full =first + ' ' + second;
+    return full;
+}
+const full = fullName('Ashikur', 'Rahman');
+// console.log(full);
+
+
+function getPrice(product){
+    if(typeof product!== 'object'){
+        return 'please provide an object';
+    }
+    console.log(typeof product);
+    const price = product.price;
+    return price ;
+}
+// const price = getPrice({name: 'Brush', price:35, color: 'blue'});
+const price = getPrice(5);
+// console.log(price);
+function getSecond(numbers) {
+    // console.log(typeof numbers); java script e array o ekti object tay array type of korse bole eta object dekhasse 
+    if(Array.isArray(numbers) === false){
+        return 'please provide an array';
+    }
+    const second = numbers[1];
+    return second;
+}
+const second = getSecond([1,2,3,4,5]);
+console.log(second);
